f17ef8a927ba18e0bac6c2f58c85d13182853b4d
Leonardo Ferreira <leodsmf@gmail.com>
2023-02-23 23:18:48 -0300

    Desafio concluido

---
+++ main.py
@@ -0,0 +1,16 @@
+from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
+
+import tweepy
+
+BRAZIL_WOE_ID = 23424768
+
+auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+trends = api.get_place_trends(BRAZIL_WOE_ID)
+
+for tweet in trends:
+    print(tweet)
